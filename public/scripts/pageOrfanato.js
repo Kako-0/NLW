058ea86beac67764451e126e8d1c3ffa7cb88096
Kayro França <k4yro01@gmail.com>
2020-10-17 21:43:35 -0300

    update mapa

--- public/scripts/pageOrfanato.js
+++ public/scripts/pageOrfanato.js
@@ -1,9 +1,22 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//Criando um mapa
+const map = L.map('mapid').setView([-2.5362386,-44.2849537], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+//Add uma camada do mapa
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+const icon = L.icon({ 
+    iconUrl: "./public/imagens/Local.svg",
+    iconSize: [38, 48],
+    iconAnchor: [29, 68],
+    popupAnchor: [80, 2]
+});
+
+const popup = L.popup({
+    closebutton: false,
+    classname: "map-popup",
+    minwidth: 240,
+    minheigth: 240
+}).setContent('Teste <a href="pageOrfanato.html?id=1" class="escolha-orfanato"> <img src="./public/imagens/Arrow-white.svg"> </a>');
+
+//Add um popup
+L.marker([-2.5362386,-44.2849537], { icon }).addTo(map).bindPopup(popup);
